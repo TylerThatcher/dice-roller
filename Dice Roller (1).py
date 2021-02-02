ce097dce37f290e18a9e658755a843d16b55fab3
TylerThatcher <77550696+TylerThatcher@users.noreply.github.com>
2021-02-02 20:42:08 +0000

    Add files via upload

---
+++ Dice Roller (1).py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[60]:
+
+
+import random
+
+def dice_roller():
+    
+    rolls = 0
+    total = 0
+    
+    dice_input = input("Please input your dice roll, for example '3d6': ").split("d")
+            
+    dice_num = int(dice_input[0])
+    dice_max = int(dice_input[1])
+    
+    while rolls < dice_num:
+        
+        dice = (random.randint(1, dice_max))
+        total += dice 
+        rolls += 1
+        print("Dice {} is: {}".format(rolls,dice))
+    
+    print("Total is: {}".format(total))
+
+        
+
+
+# In[61]:
+
+
+dice_roller()
+
